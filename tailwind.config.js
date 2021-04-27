bedf611f1cd68db727f257f50587e84a72a5d18e
Roj Vroemen <me@rojvroemen.com>
2021-04-27 15:37:54 +0200

    Setup Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  purge: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
